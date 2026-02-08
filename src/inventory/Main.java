ec19ee481b2b75131cd6fa796ecca3ffc2c063f2
SWARATHMICA <swarathmica@gmail.com>
2026-02-08 21:16:43 +0530

    feat: integrate inventory workflow in main

--- src/inventory/Main.java
+++ src/inventory/Main.java
@@ -1,6 +1,24 @@
 package inventory;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Inventory Management System");
+
+        Inventory inventory = new Inventory();
+
+        Item pushNut = new PushNut("PushNut", 10, 1);
+        Item outerNut = new OuterNut("OuterNut", 10, 1);
+        Item bolt = new Bolt("Bolt", 10, 2);
+
+        inventory.addItem(pushNut);
+        inventory.addItem(outerNut);
+        inventory.addItem(bolt);
+
+        inventory.requestItem("PushNut", 5);
+        inventory.requestItem("Bolt", 6);
+
+        System.out.println("Net worth: " + inventory.calculateNetWorth());
+        System.out.println("Most expensive item: " +
+                inventory.getMostExpensiveItem().getName());
     }
 }
